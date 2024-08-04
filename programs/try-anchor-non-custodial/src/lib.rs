dbc8ff0d9498fa06427a8400b55f483f3953a9c9
Dirga Yasa <cekingx@gmail.com>
2024-08-04 14:35:44 +0800

    feat: initialize escrow

--- programs/try-anchor-non-custodial/src/lib.rs
+++ programs/try-anchor-non-custodial/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, TokenAccount, Token};
 
 declare_id!("GGXycE7WXqPjYcib9T3U5UnMNoV1N58YtTRRXAcjb5ry");
 
@@ -6,11 +7,65 @@ declare_id!("GGXycE7WXqPjYcib9T3U5UnMNoV1N58YtTRRXAcjb5ry");
 pub mod try_anchor_non_custodial {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, x_amount: u64, y_amount: u64) -> Result<()> {
+        let escrow = &mut ctx.accounts.escrow;
+        escrow.bump = ctx.bumps.escrow;
+        escrow.authority = ctx.accounts.seller.key();
+        escrow.escrowed_x_tokens = ctx.accounts.escrowed_x_tokens.key();
+        escrow.y_amount = y_amount;
+        escrow.y_mint = ctx.accounts.y_mint.key();
+
+        anchor_spl::token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                anchor_spl::token::Transfer {
+                    from: ctx.accounts.seller_x_token.to_account_info(),
+                    to: ctx.accounts.escrowed_x_tokens.to_account_info(),
+                    authority: ctx.accounts.seller.to_account_info(),
+                }
+            ),
+            x_amount,
+        )?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    seller: Signer<'info>,
+    x_mint: Account<'info, Mint>,
+    y_mint: Account<'info, Mint>,
+    #[account(mut, constraint = seller_x_token.mint == x_mint.key() && seller_x_token.owner == seller.key())]
+    seller_x_token: Account<'info, TokenAccount>,
+    #[account(
+        init,
+        payer = seller,
+        space = Escrow::LEN,
+        seeds = ["escrow".as_bytes(), seller.key().as_ref()],
+        bump,
+    )]
+    pub escrow: Account<'info, Escrow>,
+    #[account(
+        init,
+        payer = seller,
+        token::mint = x_mint,
+        token::authority = escrow,
+    )]
+    escrowed_x_tokens: Account<'info, TokenAccount>,
+    token_program: Program<'info, Token>,
+    system_program: Program<'info, System>
+}
+
+#[account]
+pub struct Escrow {
+    authority: Pubkey,
+    bump: u8,
+    escrowed_x_tokens: Pubkey,
+    y_mint: Pubkey,
+    y_amount: u64,
+}
+
+impl Escrow {
+    pub const LEN: usize = 8 + 32 + 1 + 32 + 32 + 8;
+}
--- tests/try-anchor-non-custodial.ts
+++ tests/try-anchor-non-custodial.ts
@@ -1,16 +1,115 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { TryAnchorNonCustodial } from "../target/types/try_anchor_non_custodial";
+import * as anchor from "@coral-xyz/anchor"
+import * as splToken from "@solana/spl-token"
+import { Program } from "@coral-xyz/anchor"
+import { TryAnchorNonCustodial } from "../target/types/try_anchor_non_custodial"
 
 describe("try-anchor-non-custodial", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
+  const program = anchor.workspace.TryAnchorNonCustodial as Program<TryAnchorNonCustodial>
 
-  const program = anchor.workspace.TryAnchorNonCustodial as Program<TryAnchorNonCustodial>;
+  const payer = (provider.wallet as any).payer
+  const seller = anchor.web3.Keypair.generate()
+  const buyer = anchor.web3.Keypair.generate()
+  const escrowedXTokens = anchor.web3.Keypair.generate()
+  let x_mint;
+  let y_mint;
+  let sellers_x_token;
+  let sellers_y_token;
+  let buyer_x_token;
+  let buyer_y_token;
+  let escrow: anchor.web3.PublicKey
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  before(async() => {
+    console.log('payer', payer.publicKey.toBase58());
+    console.log('seller', seller.publicKey.toBase58());
+    console.log('buyer', buyer.publicKey.toBase58());
+    console.log('escrowedXTokens', escrowedXTokens.publicKey.toBase58());
+
+    [escrow] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        anchor.utils.bytes.utf8.encode("escrow"),
+        seller.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    x_mint = await splToken.createMint(
+      provider.connection,
+      payer,
+      payer.publicKey,
+      payer.publicKey,
+      6,
+    )
+    y_mint = await splToken.createMint(
+      provider.connection,
+      payer,
+      payer.publicKey,
+      payer.publicKey,
+      6,
+    )
+
+    sellers_x_token = await splToken.createAssociatedTokenAccount(
+      provider.connection,
+      payer,
+      x_mint,
+      seller.publicKey,
+    );
+    await splToken.mintTo(
+      provider.connection,
+      payer,
+      x_mint,
+      sellers_x_token,
+      payer,
+      1000000000,
+    )
+    sellers_y_token = await splToken.createAssociatedTokenAccount(
+      provider.connection,
+      payer,
+      y_mint,
+      seller.publicKey,
+    );
+
+    buyer_x_token = await splToken.createAssociatedTokenAccount(
+      provider.connection,
+      payer,
+      x_mint,
+      buyer.publicKey,
+    );
+    buyer_y_token = await splToken.createAssociatedTokenAccount(
+      provider.connection,
+      payer,
+      y_mint,
+      buyer.publicKey,
+    );
+    await splToken.mintTo(
+      provider.connection,
+      payer,
+      y_mint,
+      buyer_y_token,
+      payer,
+      100000
+    )
+  })
+
+  it('Initialize escrow', async () => {
+    const x_amount = new anchor.BN(40);
+    const y_amount = new anchor.BN(40);
+    const account = {
+      seller: seller.publicKey,
+      xMint: x_mint,
+      yMint: y_mint,
+      sellerXToken: sellers_x_token,
+      escrow: escrow,
+      escrowedXTokens: escrowedXTokens.publicKey,
+      tokenProgram: splToken.TOKEN_PROGRAM_ID,
+    }
+    const tx = await program.methods
+      .initialize(x_amount, y_amount)
+      .accounts(account)
+      .signers([escrowedXTokens, seller])
+      .rpc()
+    
+    console.log('tx', tx)
+  })
+})
